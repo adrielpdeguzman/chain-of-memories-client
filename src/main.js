97ec407f0cda80580c7d550b567521db32edd595
Adriel P. de Guzman <adeguzman@stratpoint.com>
2016-10-20 17:39:42 +0800

    Re-configured boilerplate
    - Removed unused files
    - Added vuex, vue-router and vue-resource
    - Added plugins for frontend

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
+/**
+ * Import plugins and dependencies for Vue.
+ */
 import Vue from 'vue';
+import Vuex from 'vuex';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+
 import App from './App';
+
+/**
+ * Configure root Vue instance.
+ */
+Vue.use(Vuex);
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+
+  ],
+});
 
 /* eslint-disable no-new */
 new Vue({
+  router,
   el: '#app',
   template: '<App/>',
   components: { App },
